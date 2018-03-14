f47bc545334474948c2721f093fe48c48beedc3e
Joseph Cheng <joseph@softworkz.net>
2018-03-14 20:47:41 +0800

    Add common Block class

---
+++ Assets/Scripts/Common/Block.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+	public BlockCategory Category { get; set; }
+
+	public int? Value { get; set; }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
+
+public enum BlockCategory
+{
+	Obstacle = 0,
+	Program = 1,
+	Terrain = 2
+}
