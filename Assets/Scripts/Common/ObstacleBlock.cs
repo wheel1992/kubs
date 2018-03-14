7569bd2e988791bfa3b6a14f38ce02ba68811ab8
Joseph Cheng <joseph@softworkz.net>
2018-03-14 22:05:15 +0800

    Add ObstacleBlock extends Block

---
+++ Assets/Scripts/Common/ObstacleBlock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBlock : MonoBehaviour, Block  {
+
+	#region Lifecycle
+
+	void Awake() 
+	{
+		Category = BlockCategory.Obstacle;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Start ();	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Update ();
+	}
+
+	#endregion
+
+	#region Public methods
+
+
+
+	#endregion
+}
+
+public enum ObstacleBlockType 
+{
+	Water = 0,
+	Mud = 1,
+	Beehive = 2
+}
