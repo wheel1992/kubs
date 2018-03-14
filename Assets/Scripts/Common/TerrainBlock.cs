3ce074c4334adad2d800ac1beda207376a545c27
Joseph Cheng <joseph@softworkz.net>
2018-03-14 22:05:27 +0800

    Add TerrainBlock extends Block

---
+++ Assets/Scripts/Common/TerrainBlock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainBlock : MonoBehaviour, Block {
+
+	#region Lifecycle
+
+	void Awake() 
+	{
+		Category = BlockCategory.Terrain;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Start ();	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Update ();
+	}
+
+	#endregion
+
+	#region Public methods
+
+
+
+	#endregion
+}
+
+public enum TerrainBlockType 
+{
+	Grass = 0,
+	Sand = 1
+}
