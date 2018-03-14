cc3ec8e83d0d591ca2b162e414a9ffa7dd2d8aad
Joseph Cheng <joseph@softworkz.net>
2018-03-14 20:47:57 +0800

    Add ProgramBlock extends Block

---
+++ Assets/Scripts/Common/ProgramBlock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgramBlock : MonoBehaviour, Block {
+
+	#region Lifecycle
+
+	void Awake() 
+	{
+		Category = BlockCategory.Program;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Start ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Update ();
+	}
+
+	#endregion
+
+	#region Public methods
+
+
+
+	#endregion
+
+}
+
+public enum ProgramBlockType 
+{
+	Forward = 0,
+	RotateLeft = 1,
+	RotateRight = 2,
+	Jump = 3,
+	ForLoop = 4,
+	End = 5
+}	
