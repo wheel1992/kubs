d32880bc6ca079cf65cb5010c7fcbf4dcea49216
Aaron Chong <aaronjhchong@hotmail.com>
2018-03-16 16:44:12 +0800

    Fix newlines

--- Assets/Scripts/Api/Character.cs
+++ Assets/Scripts/Api/Character.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
-
+public class Character : MonoBehaviour
+{
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
 	}
 }
