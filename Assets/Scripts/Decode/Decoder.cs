0368ebba8e75af93ae839f22e593773ca3409908
Aaron Chong <aaronjhchong@hotmail.com>
2018-03-16 17:00:37 +0800

    Rename files to Decoder to Encoder

--- Assets/Scripts/Decode/Decoder.cs
+++ Assets/Scripts/Decode/Decoder.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Decode : MonoBehaviour {
+public class Decoder : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- Assets/Scripts/Encode/Encoder.cs
+++ Assets/Scripts/Encode/Encoder.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Encode : MonoBehaviour {
+public class Encoder : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
