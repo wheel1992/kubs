45ddfc5c82eb741cacde53d6200fc15bbc655cca
Cheng Boon Yew <a0125474@nus.edu.sg>
2018-03-21 13:14:03 +0800

    Update Constant.cs

---
+++ Assets/Scripts/Common/Constant.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Kubs
+{
+    public class Constant
+    {
+        public static float DEFAULT_Y = 0.63f;
+
+        public static string NAME_SNAP_DROP_ZONE_PROGRAM_BLOCK = "Program_Block_SnapDropZone";
+
+        public static string TAG_BLOCK_PROGRAM = "Block_Program";
+
+    }
+}
+
