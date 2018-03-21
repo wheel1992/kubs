de8f5718c8dc75b72d18e20b184de6495b6d905b
Cheng Boon Yew <a0125474@nus.edu.sg>
2018-03-21 13:16:01 +0800

    Create forward blocks when load

---
+++ Assets/Scripts/Common/SceneLoad.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Kubs
+{
+    public class SceneLoad : MonoBehaviour
+    {
+        [SerializeField] private GameObject _forwardBlockPrefab;
+        [SerializeField] private GameObject _rotateLeftBlockPrefab;
+
+        // Use this for initialization
+        void Start()
+        {
+            CreateBlocks();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // ...
+        }
+
+        void CreateBlocks()
+        {
+            float blockSize = 1f;
+            float startX = -3f;
+            float startZ = -2f;
+
+            // let's create a few forward blocks
+            CreateForwardBlock(new Vector3(startX, Constant.DEFAULT_Y, startZ));
+            startZ += blockSize;
+            CreateForwardBlock(new Vector3(startX, Constant.DEFAULT_Y, startZ));
+            startZ += blockSize;
+            CreateForwardBlock(new Vector3(startX, Constant.DEFAULT_Y, startZ));
+        }
+
+        GameObject CreateForwardBlock(Vector3 position)
+        {
+            var forwardBlock = (GameObject) Instantiate(
+               _forwardBlockPrefab,
+               position,
+               Quaternion.identity);
+            forwardBlock.tag = Constant.TAG_BLOCK_PROGRAM;
+
+            ProgramBlock block = forwardBlock.AddComponent<ProgramBlock>();
+            block.Type = ProgramBlockType.Forward;
+
+            return forwardBlock;
+        }
+
+        GameObject CreateRotateLeftBlock(Vector3 position)
+        {
+            var rotateleftBlock = (GameObject) Instantiate(
+              _rotateLeftBlockPrefab,
+              position,
+              Quaternion.identity);
+            rotateleftBlock.tag = Constant.TAG_BLOCK_PROGRAM;
+
+            ProgramBlock block = rotateleftBlock.AddComponent<ProgramBlock>();
+            block.Type = ProgramBlockType.RotateLeft;
+
+            return rotateleftBlock;
+        }
+
+
+    }
+}
--- Assets/Scripts/Common/Vive/SnapDropZone_Block_Group.cs
+++ Assets/Scripts/Common/Vive/SnapDropZone_Block_Group.cs
@@ -3,44 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 
-public class SnapDropZone_Block_Group : MonoBehaviour {
-
-    private GameObject _programBlockZone;
-    private GameObject sphereZone;
-
-    private void Start()
+namespace Kubs
+{
+    public class SnapDropZone_Block_Group : MonoBehaviour
     {
-        _programBlockZone = transform.Find("Block_SnapDropZone").gameObject;
-        //sphereZone = transform.Find("Sphere_SnapDropZone").gameObject;
 
-        _programBlockZone.GetComponent<VRTK_SnapDropZone>().ObjectEnteredSnapDropZone += new SnapDropZoneEventHandler(DoCubeZoneSnapped);
-        _programBlockZone.GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += new SnapDropZoneEventHandler(DoCubeZoneSnapped);
-        _programBlockZone.GetComponent<VRTK_SnapDropZone>().ObjectExitedSnapDropZone += new SnapDropZoneEventHandler(DoCubeZoneUnsnapped);
-        _programBlockZone.GetComponent<VRTK_SnapDropZone>().ObjectUnsnappedFromDropZone += new SnapDropZoneEventHandler(DoCubeZoneUnsnapped);
+        private GameObject _programBlockZone;
+        //private GameObject sphereZone;
 
-        //sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectEnteredSnapDropZone += new SnapDropZoneEventHandler(DoSphereZoneSnapped);
-        //sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += new SnapDropZoneEventHandler(DoSphereZoneSnapped);
-        //sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectExitedSnapDropZone += new SnapDropZoneEventHandler(DoSphereZoneUnsnapped);
-        //sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectUnsnappedFromDropZone += new SnapDropZoneEventHandler(DoSphereZoneUnsnapped);
+        private void Start()
+        {
+            _programBlockZone = transform.Find(Constant.NAME_SNAP_DROP_ZONE_PROGRAM_BLOCK).gameObject;
+            //sphereZone = transform.Find("Sphere_SnapDropZone").gameObject;
+
+            _programBlockZone.GetComponent<VRTK_SnapDropZone>().ObjectEnteredSnapDropZone += new SnapDropZoneEventHandler(DoProgramBlockZoneSnapped);
+            _programBlockZone.GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += new SnapDropZoneEventHandler(DoProgramBlockZoneSnapped);
+            _programBlockZone.GetComponent<VRTK_SnapDropZone>().ObjectExitedSnapDropZone += new SnapDropZoneEventHandler(DoProgramBlockZoneUnsnapped);
+            _programBlockZone.GetComponent<VRTK_SnapDropZone>().ObjectUnsnappedFromDropZone += new SnapDropZoneEventHandler(DoProgramBlockZoneUnsnapped);
+
+            //sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectEnteredSnapDropZone += new SnapDropZoneEventHandler(DoSphereZoneSnapped);
+            //sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += new SnapDropZoneEventHandler(DoSphereZoneSnapped);
+            //sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectExitedSnapDropZone += new SnapDropZoneEventHandler(DoSphereZoneUnsnapped);
+            //sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectUnsnappedFromDropZone += new SnapDropZoneEventHandler(DoSphereZoneUnsnapped);
+        }
+
+        private void DoProgramBlockZoneSnapped(object sender, SnapDropZoneEventArgs e)
+        {
+            //sphereZone.SetActive(false);
+        }
+
+        private void DoProgramBlockZoneUnsnapped(object sender, SnapDropZoneEventArgs e)
+        {
+            //sphereZone.SetActive(true);
+        }
+
+        //private void DoSphereZoneSnapped(object sender, SnapDropZoneEventArgs e)
+        //{
+        //    cubeZone.SetActive(false);
+        //}
+
+        //private void DoSphereZoneUnsnapped(object sender, SnapDropZoneEventArgs e)
+        //{
+        //    cubeZone.SetActive(true);
+        //}
     }
-
-    private void DoCubeZoneSnapped(object sender, SnapDropZoneEventArgs e)
-    {
-        //sphereZone.SetActive(false);
-    }
-
-    private void DoCubeZoneUnsnapped(object sender, SnapDropZoneEventArgs e)
-    {
-        //sphereZone.SetActive(true);
-    }
-
-    //private void DoSphereZoneSnapped(object sender, SnapDropZoneEventArgs e)
-    //{
-    //    cubeZone.SetActive(false);
-    //}
-
-    //private void DoSphereZoneUnsnapped(object sender, SnapDropZoneEventArgs e)
-    //{
-    //    cubeZone.SetActive(true);
-    //}
 }
+
