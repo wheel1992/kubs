19092772c183ee7974b5c23af3e396e5795151f9
Aaron Chong <aaronjhchong@hotmail.com>
2018-03-24 15:56:18 +0800

    Add enum: Animations

---
+++ Assets/Scripts/Character/Animations.cs
@@ -0,0 +1,28 @@
+﻿namespace Kubs
+{
+	public enum Animations
+	{
+		NewState  = 0,
+		Idle      = 1,
+		Alert     = 2,
+		Victory   = 3,
+		Block     = 4,
+		Damage    = 5,
+		Die1      = 6,
+		Die2      = 7,
+		Dash      = 8,
+		Jump      = 9,
+		Sit       = 10,
+		ATK1      = 11,
+		ATK2      = 12,
+		ATK3      = 13,
+		ATK4      = 14,
+		Move      = 15,
+		Move_R    = 16,
+		Move_L    = 17,
+		Walk      = 18,
+		IdleA     = 19,
+		NormalATK = 20,
+		Sleep     = 21
+	}
+}
