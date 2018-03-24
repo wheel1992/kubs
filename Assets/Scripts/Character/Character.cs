852b46030262f38e38edd99d8cc4c93483d3a1c9
Aaron Chong <aaronjhchong@hotmail.com>
2018-03-24 13:29:02 +0800

    Refactor forward into Enqueue, Start and Update

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -31,21 +31,7 @@ public class Character : MonoBehaviour
 			switch (_type)
 			{
 				case ProgramBlockType.Forward:
-					// https://answers.unity.com/questions/8318/throwing-object-with-acceleration-equationscript.html
-					// calculate current time within our lerping time range
-					float cTime = Time.time * 0.5f;
-					// calculate straight-line lerp position:
-					Vector3 currentPos = Vector3.Lerp(startPos, endPos, cTime);
-					// add a value to Y, using Sine to give a curved trajectory in the Y direction
-					currentPos.y += trajectoryHeight * Mathf.Sin(Mathf.Clamp01(cTime) * Mathf.PI);
-					DebugLog(transform.position);
-					if (transform.position == endPos)
-					{
-						DebugLog("end");
-						_isAnimating = false;
-					}
-					// finally assign the computed position to our gameObject:
-					transform.position = currentPos;
+					Forward_Update();
 					break;
 				default:
 					break;
@@ -55,6 +41,16 @@ public class Character : MonoBehaviour
 
 	public bool Forward()
 	{
+		return _isAnimating ? Forward_Enqueue() : Forward_Start();
+	}
+
+	private bool Forward_Enqueue()
+	{
+		return false;
+	}
+
+	private bool Forward_Start()
+	{
 		startPos = new Vector3(0, 0.5f, 0);
 		endPos = new Vector3(0, 0.5f, 4);
 		trajectoryHeight = 1;
@@ -62,7 +58,26 @@ public class Character : MonoBehaviour
 		_type = ProgramBlockType.Forward;
 		_isAnimating = true;
 
-		return false;
+		return true;
+	}
+
+	private void Forward_Update()
+	{
+		// https://answers.unity.com/questions/8318/throwing-object-with-acceleration-equationscript.html
+		// calculate current time within our lerping time range
+		float cTime = Time.time * 0.5f;
+		// calculate straight-line lerp position:
+		Vector3 currentPos = Vector3.Lerp(startPos, endPos, cTime);
+		// add a value to Y, using Sine to give a curved trajectory in the Y direction
+		currentPos.y += trajectoryHeight * Mathf.Sin(Mathf.Clamp01(cTime) * Mathf.PI);
+		DebugLog(transform.position);
+		if (transform.position == endPos)
+		{
+			DebugLog("end");
+			_isAnimating = false;
+		}
+		// finally assign the computed position to our gameObject:
+		transform.position = currentPos;
 	}
 
 	public bool Jump()
