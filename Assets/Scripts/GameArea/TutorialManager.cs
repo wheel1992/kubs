3c64cd971a872bed7bf186b2604405079e999798
Aaron Chong <aaronjhchong@hotmail.com>
2018-03-27 19:26:41 +0800

    Add script: tutorial manager

---
+++ Assets/Scripts/GameArea/TutorialManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kubs
+{
+	public class TutorialManager : MonoBehaviour
+	{
+		void Start()
+		{
+		}
+	}
+}
