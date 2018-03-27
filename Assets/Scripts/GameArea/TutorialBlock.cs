1435b909fcd0199a45cb3a39a8c2a04e205511fe
Aaron Chong <aaronjhchong@hotmail.com>
2018-03-27 19:28:29 +0800

    Add script: tutorial block

---
+++ Assets/Scripts/GameArea/TutorialBlock.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kubs
+{
+	public class TutorialBlock : MonoBehaviour
+	{
+		void Start()
+		{
+		}
+	}
+}
