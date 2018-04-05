6a9445666aa23e46ca09e6bf259eecff26d0398e
Joseph A0125474 <josephcheng1992@gmail.com>
2018-04-05 13:54:42 +0800

    Add ForLoopProgramBlock

---
+++ Assets/Scripts/Common/ForLoopProgramBlock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoopProgramBlock : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    GameObject GetCounterAreaGameObject()
+    {
+        return transform.Find("ForStartCounterArea").gameObject;
+    }
+    GameObject GetCounterAddGameObject()
+    {
+        return GetCounterAreaGameObject().transform.Find("CounterAddArea").gameObject;
+    }
+    GameObject GetCounterMinusGameObject()
+    {
+        return GetCounterAreaGameObject().transform.Find("CounterMinusArea").gameObject;
+    }
+    GameObject GetCounterNumberGameObject()
+    {
+        return GetCounterAreaGameObject().transform.Find("CounterNumberArea").gameObject;
+    }
+}
