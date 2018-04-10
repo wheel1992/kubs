3bb5e2dead84b7e67462c76f0e5410d193baa335
Joseph <josephcheng1992@gmail.com>
2018-04-10 20:29:31 +0800

    Enable background music

--- Assets/Scripts/Common/SceneLoad.cs
+++ Assets/Scripts/Common/SceneLoad.cs
@@ -33,8 +33,8 @@ namespace Kubs
         void Awake()
         {
             _mAudioSource = GetComponent<AudioSource>();
-            _mAudioSource.Stop();
-            //_mAudioSource.Play();
+            //_mAudioSource.Stop();
+            _mAudioSource.Play();
         }
         void Start()
         {
