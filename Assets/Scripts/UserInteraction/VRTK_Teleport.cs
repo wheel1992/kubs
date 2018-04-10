a221cb6df0ef9cd4cb7d4cb52ddab8a263ce16b1
Joseph <josephcheng1992@gmail.com>
2018-04-10 20:14:20 +0800

    Disable teleport if y > 0.1

---
+++ Assets/Scripts/UserInteraction/VRTK_Teleport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+namespace Kubs
+{
+	public class VRTK_Teleport : VRTK_BasicTeleport
+	{
+		public override bool ValidLocation(Transform target, Vector3 destinationPosition)
+		{
+			if (destinationPosition.y > 0.1)
+			{
+				return false;
+			}
+
+			return base.ValidLocation(target, destinationPosition);
+		}
+	}
+}
