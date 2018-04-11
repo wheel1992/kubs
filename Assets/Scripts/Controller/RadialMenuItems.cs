3134fe5d90c4ce5357e894098c10cd8ec39172dc
Joseph <josephcheng1992@gmail.com>
2018-04-11 18:07:49 +0800

    Add RadialMenuItems

---
+++ Assets/Scripts/Controller/RadialMenuItems.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Kubs
+{
+    public class RadialMenuItems : MonoBehaviour
+    {
+        public void OnOptionMenuClick()
+        {
+			Debug.Log("RadialMenuItems OnOptionMenuClick:");
+        }
+		public void OnOptionSelectClick()
+        {
+			Debug.Log("RadialMenuItems OnOptionSelectClick:");
+			Debug.Log("RadialMenuItems OnOptionSelectClick: " + transform.root.name);
+        }
+		public void OnOptionTeleportClick()
+        {
+			Debug.Log("RadialMenuItems OnOptionTeleportClick:");
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    }
+}
+
