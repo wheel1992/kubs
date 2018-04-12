0c9f5b82af9ec3e3ee07333cc7f88fd8adfa99e2
Terence Lim <terencelimjc@gmail.com>
2018-04-12 20:39:25 +0800

    Modified UIProgramBlockHints to UXScene

--- Assets/Scripts/UserInteraction/UIProgramBlockHints.cs
+++ Assets/Scripts/UserInteraction/UIProgramBlockHints.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 
-public class UIProgramBlockHints : MonoBehaviour {
+public class UIProgramBlockHints : MonoBehaviour
+{
 
     private bool _onHover = false;
     private bool _animating = false;
@@ -16,36 +17,55 @@ public class UIProgramBlockHints : MonoBehaviour {
     [SerializeField] private GameObject GrassPrefab;
     [SerializeField] private GameObject HolePrefab;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         //_onHover = true;
-		if(this.gameObject.GetComponent<VRTK_InteractableObject>() != null) {
+        if (this.gameObject.GetComponent<VRTK_InteractableObject>() != null)
+        {
             var io = gameObject.GetComponent<VRTK_InteractableObject>();
             io.InteractableObjectTouched += new InteractableObjectEventHandler(HandleOnTouch);
             io.InteractableObjectGrabbed += new InteractableObjectEventHandler(HandleOnGrab);
-            //io.InteractableObjectUntouched += new InteractableObjectEventHandler(HandleUnTouch);
+            io.InteractableObjectUntouched += new InteractableObjectEventHandler(HandleUnTouch);
             //if(gameObject.CompareTag("Block_Program"))
             //{
             currentGameObject = this.gameObject;
             //Debug.Log("UIProgramBlockHints Start Function: " + currentGameObject.name );
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(_onHover)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_onHover)
         {
-            if(currentGameObject.CompareTag("Block_Program") && !_animating)
+            if (currentGameObject.CompareTag("Block_Program"))
             {
                 ProgramBlockType programBlockType = currentGameObject.GetComponent<ProgramBlock>().Type;
                 //Debug.Log("UIProgramBlockHints Update Function: " + currentGameObject.name );
-                StartCoroutine(CreateProgramBlockUIHint(programBlockType));
-                _animating = true;
+                if (UIProgramBlockHintsPointer != null)
+                {
+                    UIProgramBlockHintsPointer.SetActive(true);
+                    if (_character == null) CreateChar(UIProgramBlockHintsPointer);
+                    else if (!_animating)
+                    {
+                        _character.Reset();
+                        _character.gameObject.SetActive(true);
+                        _animating = true;
+                        StartCoroutine(MoveCharacterAfterTwoFrames(programBlockType));
+                    }
+                }
+                else UIProgramBlockHintsPointer = CreateProgramBlockUIHint(programBlockType);
             }
-        } else
+        }
+        else
         {
-            if (UIProgramBlockHintsPointer != null)
-                GameObject.Destroy(UIProgramBlockHintsPointer);
+            if (GameObject.Find("UIProgramBlockHints"))
+            {
+                StopCoroutine("MoveCharacterAfterTwoFrames");
+                UIProgramBlockHintsPointer = GameObject.Find("UIProgramBlockHints");
+                UIProgramBlockHintsPointer.SetActive(false);
+            }
         }
     }
 
@@ -79,50 +99,46 @@ public class UIProgramBlockHints : MonoBehaviour {
         _onHover = false;
     }
 
-    private IEnumerator CreateProgramBlockUIHint(ProgramBlockType programBlockType)
+    private GameObject CreateProgramBlockUIHint(ProgramBlockType programBlockType)
     {
-        GameObject UIProgramBlockHints = new GameObject();
-        if(UIProgramBlockHintsPointer == null)
+        GameObject UIProgramBlockHints = null;
+        if (UIProgramBlockHintsPointer == null)
         {
             switch (programBlockType)
             {
                 case ProgramBlockType.Forward:
                     UIProgramBlockHints = CreateMiniGameArea(2, "Program_Block_SnapDropZone_Clone_Forward");
-                    UIProgramBlockHintsPointer = UIProgramBlockHints;
                     break;
                 case ProgramBlockType.Jump:
                     UIProgramBlockHints = CreateMiniGameArea(3, "Program_Block_SnapDropZone_Clone_Jump");
-                    UIProgramBlockHintsPointer = UIProgramBlockHints;
                     break;
                 case ProgramBlockType.RotateLeft:
                     UIProgramBlockHints = CreateMiniGameArea(1, "Program_Block_SnapDropZone_Clone_RotateLeft");
-                    UIProgramBlockHintsPointer = UIProgramBlockHints;
                     break;
                 case ProgramBlockType.RotateRight:
                     UIProgramBlockHints = CreateMiniGameArea(1, "Program_Block_SnapDropZone_Clone_RotateRight");
-                    UIProgramBlockHintsPointer = UIProgramBlockHints;
                     break;
                 default:
                     break;
             }
         }
         //Instantiate Char
-        _character = CreateChar(UIProgramBlockHints);
+        // _character = CreateChar(UIProgramBlockHints);
         //Make character move
-        StartCoroutine(MoveCharacterAfterTwoFrames(programBlockType));
-        _animating = false;
-        //Destroy(GetCharacterFromGameArea(UIProgramBlockHints), 0);
-        yield return new WaitForSeconds(3);
+        // StartCoroutine(MoveCharacterAfterTwoFrames(programBlockType));
 
-        _character.Reset();
-        StartCoroutine(MoveCharacterAfterTwoFrames(programBlockType));
+        //Destroy(GetCharacterFromGameArea(UIProgramBlockHints), 0);
+        // yield return new WaitForSeconds(3);
+
+        // _character.Reset();
+        // StartCoroutine(MoveCharacterAfterTwoFrames(programBlockType));
+
+        return UIProgramBlockHints;
+        // _animating = false;
     }
 
     private IEnumerator MoveCharacterAfterTwoFrames(ProgramBlockType programBlockType)
     {
-        yield return null;
-        yield return null;
-
         switch (programBlockType)
         {
             case ProgramBlockType.Forward:
@@ -140,35 +156,38 @@ public class UIProgramBlockHints : MonoBehaviour {
             default:
                 break;
         }
+        //Start here
+        //_character.gameObject.SetActive(false);
+        //_character.Reset();
+        yield return new WaitForSecondsRealtime(3);
+        Debug.Log("Wait For 3 Seconds");
+        _animating = false;
     }
 
     private GameObject CreateMiniGameArea(int numTerrain, string programBlockTypeOnFieldString)
     {
-        GameObject UIProgramBlockHints = Instantiate(new GameObject(), currentGameObject.transform, false);
-        GameObject programBlockTypeOnField = GameObject.Find(programBlockTypeOnFieldString);
+        GameObject UIProgramBlockHints = new GameObject();
+        UIProgramBlockHints.transform.SetParent(currentGameObject.transform);
         UIProgramBlockHints.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
-        UIProgramBlockHints.transform.position = programBlockTypeOnField.transform.position + new Vector3(0, 1, 0);
-        //Debug.Log("UIProgramBlockHints CreateMiniGameArea CurrentGameObject: " + currentGameObject.name);
-        //Debug.Log("UIProgramBlockHints CreateMiniGameArea UIProgramBlockHints BeforeTransform Transform: " + UIProgramBlockHints.transform.localPosition);
-        //Debug.Log("UIProgramBlockHints CreateMiniGameArea UIProgramBlockHints ProgramBlockTypeOnField Transform: " + programBlockTypeOnField.transform.position);
-        //Debug.Log("UIProgramBlockHints CreateMiniGameArea UIProgramBlockHints Transform: " + UIProgramBlockHints.transform.localPosition);
+        UIProgramBlockHints.transform.position = currentGameObject.transform.position + new Vector3(0, 1, 0);
         UIProgramBlockHints.name = "UIProgramBlockHints";
+        UIProgramBlockHintsPointer = UIProgramBlockHints;
         for (int numBlock = 0; numBlock < numTerrain; numBlock++)
         {
-            GameObject cube = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), UIProgramBlockHints.transform, false);
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube.name = "MiniTerrain" + numBlock;
-            //cube.transform.SetParent(UIProgramBlockHints.transform);
-            //cube.transform.localPosition = new Vector3(0, ((0.5f+scaleFactor*2)/scaleFactor), 0);
+            cube.transform.SetParent(UIProgramBlockHints.transform);
+            cube.transform.localPosition = new Vector3(0, ((0.5f + scaleFactor * 2) / scaleFactor), 0);
             cube.tag = "Cubes";
         }
         //GameObject miniCharObject = Instantiate(CharacterPrefab, UIProgramBlockHints.transform);
         //miniCharObject.tag = "UICharacter";
-        ArrangeGameArea(UIProgramBlockHints);
-        var terrainReplacer = UIProgramBlockHints.AddComponent<TerrainReplacer>();
+        ArrangeGameArea(UIProgramBlockHintsPointer);
+        var terrainReplacer = UIProgramBlockHintsPointer.AddComponent<TerrainReplacer>();
         SetNamedPrefabs(terrainReplacer);
         //UIProgramBlockHints.transform.localEulerAngles = new Vector3(0, 45f, 0);
         //Debug.Log("End");
-        return UIProgramBlockHints;
+        return UIProgramBlockHintsPointer;
     }
 
     private void ArrangeGameArea(GameObject area)
@@ -205,12 +224,12 @@ public class UIProgramBlockHints : MonoBehaviour {
                         //    Debug.Log("UIProgramBlockHints.ArrangeGameArea: GetCharacterFromGameArea returns null");
                         break;
                     case 2:
-                         cubeOne = cubeCollection[0];
-                         cubeTwo = cubeCollection[1];
-                         cubeOne.transform.localPosition = new Vector3(0, 0, -0.5f);
-                         cubeOne.tag = "Grass";
-                         cubeTwo.transform.localPosition = new Vector3(0, 0, 0.5f);
-                         cubeTwo.tag = "Grass";
+                        cubeOne = cubeCollection[0];
+                        cubeTwo = cubeCollection[1];
+                        cubeOne.transform.localPosition = new Vector3(0, 0, -0.5f);
+                        cubeOne.tag = "Grass";
+                        cubeTwo.transform.localPosition = new Vector3(0, 0, 0.5f);
+                        cubeTwo.tag = "Grass";
                         //if (GetCharacterFromGameArea(area) != null)
                         // {
                         //     miniChar = GetCharacterFromGameArea(area);
@@ -258,7 +277,7 @@ public class UIProgramBlockHints : MonoBehaviour {
         {
             foreach (Transform child in area.transform)
             {
-                if(child.tag == "UICharacter")
+                if (child.tag == "UICharacter")
                 {
                     miniChar = child.gameObject.GetComponent<Character>();
                 }
@@ -270,11 +289,12 @@ public class UIProgramBlockHints : MonoBehaviour {
     private List<GameObject> GetCubesFromGameArea(GameObject area)
     {
         List<GameObject> cubeCollection = new List<GameObject>();
-        if(area != null)
+        if (area != null)
         {
+            Debug.Log("Area Null");
             foreach (Transform child in area.transform)
             {
-                if (child.tag == "Cubes" || child.tag == "Grass" || child.tag == "Hole")
+                if (child.tag == "Cubes" || child.tag == "Grass" || child.tag == "Hole" || child.name.Contains("Grass") || child.name.Contains("Hole"))
                 {
                     cubeCollection.Add(child.gameObject);
                 }
@@ -295,11 +315,14 @@ public class UIProgramBlockHints : MonoBehaviour {
 
     private Character CreateChar(GameObject area)
     {
-        Character miniChar = Instantiate(CharacterPrefab, area.transform).GetComponent<Character>();
-        miniChar.name = "MiniCharacter";
-        miniChar.tag = "UICharacter";
-        miniChar.transform.localPosition = GetCubesFromGameArea(area)[0].transform.localPosition + new Vector3(0, 1f, 0);
-        return miniChar;
+        _character = Instantiate(CharacterPrefab, area.transform).GetComponent<Character>();
+        _character.gameObject.SetActive(false);
+        _character.name = "MiniCharacter";
+        _character.tag = "UICharacter";
+        Debug.Log(area.name);
+        _character.transform.localPosition = GetCubesFromGameArea(area)[0].transform.localPosition + new Vector3(0, 1f, 0);
+        _character.gameObject.SetActive(true);
+        return _character;
     }
 
     //private void AssignBlockType(string blockTypeInString, GameObject cube)
