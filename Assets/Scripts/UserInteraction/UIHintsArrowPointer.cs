faaa55e47e3d74c72fdd4b48f0fc6ebcdd8c70e6
Joseph <josephcheng1992@gmail.com>
2018-04-17 13:29:28 +0800

    Add StartFloat

--- Assets/Scripts/UserInteraction/UIHintsArrowPointer.cs
+++ Assets/Scripts/UserInteraction/UIHintsArrowPointer.cs
@@ -4,36 +4,68 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using VRTK;
 
-public class UIHintsArrowPointer : MonoBehaviour {
+public class UIHintsArrowPointer : MonoBehaviour
+{
 
     //[SerializeField] GameObject ArrowPrefab;
-    private float arrowMoveSpeed = 0.1f;
-    private float xPosAbove = 1.0f;
-    private bool _onShow = false;
-    private float distAccuracy;
-    private Vector3 startPos;
-    private Vector3 endPos;
-    private Vector3 velocity = new Vector3(0, 0, 0);
+    // private float arrowMoveSpeed = 0.0001f;
+    // private float xPosAbove = 2.0f;
+    // private bool _onShow = false;
+    // private float distAccuracy;
+    // private Vector3 startPos;
+    // private Vector3 endPos;
+    // private float incrementor;
+
+
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
 
     // Use this for initialization
     void Start()
     {
-        distAccuracy = arrowMoveSpeed;
-        endPos = this.gameObject.transform.position;
-        startPos = endPos + new Vector3(0, xPosAbove, 0);
+        // incrementor = 0.01f;
+        // distAccuracy = arrowMoveSpeed;
+        // endPos = this.gameObject.transform.position;
+        // startPos = endPos + new Vector3(0, xPosAbove, 0);
+
+        posOffset = transform.position;
+        StartCoroutine(StartFloat());
     }
 
     // Update is called once per frame
-    void Update()
+    // void Update()
+    // {
+    //     incrementor += Time.deltaTime;
+    //     if (Vector3.Distance(this.gameObject.transform.position, endPos) < distAccuracy)
+    //     {
+    //         this.gameObject.transform.position = startPos;
+    //         incrementor = 0.01f;
+    //     }
+    //     else
+    //     {
+    //         this.gameObject.transform.position = Vector3.Lerp(this.gameObject.transform.position, endPos, incrementor);
+    //     }
+    // }
+
+    private IEnumerator StartFloat()
     {
-        if (Vector3.Distance(this.gameObject.transform.position, endPos) < distAccuracy)
-            {
-                this.gameObject.transform.position = startPos;
-            }
-        else
-            {
-                this.gameObject.transform.position = Vector3.SmoothDamp(this.gameObject.transform.position, endPos, ref velocity, Time.deltaTime * arrowMoveSpeed);
-            }
+        while (true)
+        {
+            // Spin object around Y-Axis
+            // transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+            // Float up/down with a Sin()
+            tempPos = posOffset;
+            tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+            transform.position = tempPos;
+
+            yield return null;
+        }
     }
 
     //public GameObject CreateArrowPointer(Vector3 localPos)
