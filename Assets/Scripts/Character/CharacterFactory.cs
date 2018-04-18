0c899bc5f9610c7c1bc4291f93982f6c9872739e
Aaron Chong <aaronjhchong@hotmail.com>
2018-04-18 12:35:35 +0800

    Add CharacterFactory and CharacterManager

---
+++ Assets/Scripts/Character/CharacterFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kubs
+{
+	public class CharacterFactory : MonoBehaviour
+	{
+		public GameObject prefab;
+
+		// Use this for initialization
+		void Start()
+		{
+			var placeholder = transform.GetChild(0).gameObject;
+			placeholder.SetActive(false);
+
+			var go = Instantiate(prefab, transform);
+			var character = go.AddComponent<Character>();
+			character.transform.localPosition = placeholder.transform.localPosition;
+			character.showPopup = true;
+			character.stageTriggerManager = GameObject.FindObjectOfType<StageTriggerManager>();
+			character.tutorialManager = GameObject.FindObjectOfType<TutorialManager>();
+		}
+	}
+}
---
+++ Assets/Scripts/Character/CharacterManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kubs
+{
+	public class CharacterManager : MonoBehaviour
+	{
+		public static GameObject characterPrefab;
+		
+		public GameObject[] characterPrefabs;
+	}
+}
